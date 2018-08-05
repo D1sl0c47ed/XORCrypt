dbad91c892a9838490b2e201e5d1bfd0734e2b81
Andrey Samoilov <heyhahch@outlook.com>
2018-08-05 14:01:41 +0300

    Added multiline capability

--- XORCrypt/Program.cs
+++ XORCrypt/Program.cs
@@ -10,54 +10,87 @@ namespace XORCrypt
     {
         static void Main(string[] args)
         {
-            
+
+            string plainString = null;
+            List<string> plainList = new List<string>();
             //Ask user to input plaintext.
-            Console.Write("Please enter plaintext:");
-            string plainText = Console.ReadLine();
+            int line = 1;
+            do
+            {
+              
+                Console.Write("Please enter line {0}:",line);
 
-            //Encrypt
+                plainString = Console.ReadLine();
+                if (plainString != "exit")
+                {
+                    plainList.Add(plainString);
+                    line++;
+                }
+                else
+                {
+                    break;
+                }
 
-            //Create an array of chars for a key
-            char[] cryptKey = new char[plainText.Length];
-            //Create an array of chars for storing CypherText in process.
-            char[] cypherChar = new char[plainText.Length];
+            } while (true);
+
+            List<string> CryptKeys = new List<string>();
+            List<string> CypherList = new List<string>();
+
             //Create an instance of random class for key generation.
             Random random = new Random();
-
-            //Iterate through every character of the string.
-            for (int i = 0; i < plainText.Length; i++)
+            foreach (string PlainText in plainList)
             {
-                //Create a value in range of ASCII 7-bit.
-                cryptKey[i] = (char)random.Next(0, 128);
-                //Perform binary XOR on a char from plaintext and a value from key.
-                cypherChar[i] = (char)(plainText[i] ^ cryptKey[i]);
 
+                char[] cryptKey = new char[PlainText.Length]; // An array of chars for a key
+                char[] cypherChar = new char[PlainText.Length]; // An array of chars for cypherText
+
+                
+
+                //Iterate through every character of the string.
+                for (int i = 0; i < PlainText.Length; i++)
+                {
+                    //Assign a pseudo-random value in range of ASCII 7-bit.
+                    cryptKey[i] = (char)random.Next(0, 128);
+                    //Perform binary XOR on char from PlainText and a value from cryptKey.
+                    cypherChar[i] = (char)(PlainText[i] ^ cryptKey[i]);
+                }
+
+                //Add results to lists
+                CryptKeys.Add(new string(cryptKey));
+                CypherList.Add(new string(cypherChar));
+                
             }
-            //Store result as a string.
-            string cypherText = new string(cypherChar);
-            
-
-            Console.WriteLine("Cyphertext: {0}",cypherText); //Output cyphertext.
-
-            //Decrypt
-
-            //Create a char array for storing decrypted chars
-            char[] decryptedChar = new char[cypherText.Length];
-
-            //Iterate through every char of the string.
-            for (int i = 0; i < cypherText.Length; i++)
+            // Output CypherText
+            foreach (string CypherText in CypherList)
             {
-                //Perform binary XOR on the cyphertext and the key.
-                //If a bit from cyphertext = 0, and bit from cryptKey = 0, decrypted bit = 0; 1:1 - 0; 0:1 - 1; 1:0 - 1.
-                decryptedChar[i] = (char)(cypherText[i] ^ cryptKey[i]);
+                Console.WriteLine("Cyphertext: {0}", CypherText);
             }
-            //Store result as a string.
-            string decryptedText = new string(decryptedChar);
-
-            Console.WriteLine("Decrypted text: {0}", decryptedText); //Output deciphered text.
-
             Console.ReadLine();
 
+      
+            //Decrypt
+            for(int iList=0; iList<CryptKeys.Count; iList++)
+            {
+                //Set iterable items.
+                char[] CryptKey = CryptKeys[iList].ToCharArray();
+                char[] CypherText = CypherList[iList].ToCharArray();
+
+                char[] decryptedChar = new char[CypherText.Length];
+                
+                //Iterate through every char of the string.
+                for (int i = 0; i < CypherText.Length; i++)
+                {
+                    //Perform binary XOR on the cyphertext and the key.
+                    //If a bit from cyphertext = 0, and bit from cryptKey = 0, decrypted bit = 0; 1:1 - 0; 0:1 - 1; 1:0 - 1.
+                    decryptedChar[i] = (char)(CypherText[i] ^ CryptKey[i]);
+                }
+                //Store result as a string.
+                string decryptedText = new string(decryptedChar);
+
+                Console.WriteLine("Decrypted text: {0}", decryptedText); //Output deciphered text.
+
+            }
+            Console.ReadLine();
         }
     }
 }
